7247563c579821ecddba291200407c390b62fce9
Hamza <idries.hamza@gmail.com>
2020-09-14 11:44:49 +0300

    git test 02

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flexoclock',
         home: Scaffold(
           backgroundColor: Colors.purple,
