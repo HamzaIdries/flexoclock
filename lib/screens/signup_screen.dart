92afdac6dc18f538a6397dfa2c85d61be9094198
Khalid <2018980035@ses.yu.edu.jo>
2020-09-15 19:59:26 +0300

    finish flexible card, started working on fixed cards

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
-
-class SignupScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        width: ,
-      ),
-    );
-  }
-}
+//import 'package:flutter/material.dart';
+//
+//class SignupScreen extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    Size size = MediaQuery.of(context).size;
+//    return Scaffold(
+//      body: Container(
+//        width: ,
+//      ),
+//    );
+//  }
+//}
--- lib/testing.dart
+++ lib/testing.dart
@@ -1,25 +1,25 @@
-import 'package:flexoclock/components/login_background.dart';
-import 'package:flexoclock/screens/login_screen.dart';
-import 'package:flexoclock/screens/welcome_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flexoclock/components/text_field.dart';
-import 'package:flexoclock/components/text_field_comp.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-void main() {
-  runApp(Testing());
-}
-
-class Testing extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flexoclock',
-      home: Scaffold(
-        body:
-      ),
-    );
-  }
-}
+//import 'package:flexoclock/components/login_background.dart';
+//import 'package:flexoclock/screens/login_screen.dart';
+//import 'package:flexoclock/screens/welcome_screen.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
+//import 'package:flexoclock/components/text_field.dart';
+//import 'package:flexoclock/components/text_field_comp.dart';
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//
+//void main() {
+//  runApp(Testing());
+//}
+//
+//class Testing extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      debugShowCheckedModeBanner: false,
+//      title: 'Flexoclock',
+//      home: Scaffold(
+//        body:
+//      ),
+//    );
+//  }
+//}
