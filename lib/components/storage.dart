f07713788e8ef6eb5a87d113c4fa97d0d84e048b
Hamza <idries.hamza@gmail.com>
2020-09-16 14:08:56 +0300

    shortcut email keyboard

---
+++ lib/components/storage.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+
+class Storage{
+  Future<String> get LocalPath async{
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get LocalFile async{
+    final path = await LocalPath;
+    return File('$path/db.txt');
+  }
+
+  Future<String> readData() async{
+    try{
+      final file = await LocalFile;
+      String body = await file.readAsString();
+
+      return body;
+    } catch(e){
+      return e.toString();
+    }
+  }
+
+  Future<File> writeData(String data) async{
+    final file = await LocalFile;
+    return file.writeAsString('$data');
+  }
+
+}
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flexoclock/components/top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flexoclock/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -8,7 +10,20 @@ class ProfileScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body:
+      body: SafeArea(
+        child: Stack(
+            children: [
+              ClipPath(
+                //clipper: CustomClipper(),
+                child: Container(
+                  height: 400,
+                  child: Image.asset('${kImagePath}cliff.png',),
+                ),
+              ),
+              TopBar(),
+            ],
+          ),
+      ),
     );
   }
 }
